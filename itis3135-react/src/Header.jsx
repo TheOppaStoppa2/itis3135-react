db05d27d219c917353238a9323b70d445d83ff11
TheOppaStoppa2 <skumar51@charlotte.edu>
2025-11-03 20:26:53 -0500

    classwork

---
+++ itis3135-react/src/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return <header> 
+        <h1>
+            Home page of mine
+        </h1>
+        <nav>
+            
+        </nav>
+    </header>;
+
+}
