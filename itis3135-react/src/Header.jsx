96a8bf24b7baf6b8cb083f7edf97bc69bd69048b
TheOppaStoppa2 <skumar51@charlotte.edu>
2025-11-08 13:51:43 -0500

    Lots of page works

--- itis3135-react/src/Header.jsx
+++ itis3135-react/src/Header.jsx
@@ -1,11 +1,19 @@
-export default function Header(){
-    return <header> 
-        <h1>
-            Home page of mine
-        </h1>
-        <nav>
-            
-        </nav>
-    </header>;
-
+export default function Header() {
+  return (
+    <header>
+      <h1>Sankalp Kumar’s Super Kangaroo</h1>
+      <h2>ITIS 3135</h2>
+      <nav>
+        <ul className="primary-nav">
+          <li><a href="/">Home</a></li>
+          <li><a href="/introduction">Introduction</a></li>
+          <li><a href="/contract">Contract</a></li>
+        </ul>
+        <ul className="secondary-nav">
+          <li><a href="https://yourusername.github.io/itis3135/">Main Site</a></li>
+          <li><a href="https://yourusername.github.io/itis3135-react/">React3</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
--- itis3135-react/src/main.jsx
+++ itis3135-react/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Home from './pages/Home'
+import Introduction from './pages/Introduction'
+import Contract from './pages/Contract'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="introduction" element={<Introduction />} />
+          <Route path="contract" element={<Contract />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
