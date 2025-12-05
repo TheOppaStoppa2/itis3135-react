90fa4fb7481e74f35972b2256d0901543fab48f0
TheOppaStoppa2 <skumar51@charlotte.edu>
2025-12-05 00:07:31 -0500

    k

---
+++ itis3135-react/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h2>ITIS 3135 Course Page</h2>
+      <h3 style={{ fontStyle: "italic" }}>
+        
+      </h3>
+    </>
+  );
+}
+
+export default App;
